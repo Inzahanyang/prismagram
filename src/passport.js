077cb6014216381ea800fc148efa2040f6140ef5
inzahan <inzahanyang@gmail.com>
2020-03-03 14:47:34 +0900

    Passport JWT part three

--- src/passport.js
+++ src/passport.js
@@ -1,10 +1,6 @@
-import dotenv from "dotenv";
-import path from "path";
-dotenv.config({ path: path.resolve(__dirname, ".env") });
-
 import passport from "passport";
 import { Strategy, ExtractJwt } from "passport-jwt";
-import { userInfo } from "os";
+import { prisma } from "../generated/prisma-client";
 
 const jwtOptions = {
   jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
@@ -24,4 +20,13 @@ const verifyUser = async (payload, done) => {
   }
 };
 
+export const authenticateJwt = (req, res, next) =>
+  passport.authenticate("jwt", { session: false }, (error, user) => {
+    if (user) {
+      req.user = user;
+    }
+    next();
+  })(req, res, next);
+
 passport.use(new Strategy(jwtOptions, verifyUser));
+passport.initialize();
